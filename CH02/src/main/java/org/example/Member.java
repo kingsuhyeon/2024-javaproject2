b533ab68524b23515edaa4ca295a81dd85b05e8b
kingsuhyeon <dvlproh@gmail.com>
2024-09-03 14:21:43 +0900

    자바프로젝트 시작

---
+++ CH02/src/main/java/org/example/Member.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String username;
+
+    @Column(name = "age")
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
